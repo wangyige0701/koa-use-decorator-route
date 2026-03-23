7f14de78eeaf1940c86a716698b67ee49b1f734f
wangyige0701 <1776686515@qq.com>
2026-03-23 21:45:53 +0800

    条件控制器完善

--- src/decorators/if.ts
+++ src/decorators/if.ts
@@ -16,6 +16,7 @@ class IFDecorator {
 		if (this.condition) {
 			return this;
 		}
+		this.condition = true;
 		this.assertDecorator(decorator);
 		return this;
 	}
--- src/test/controller/IfController.ts
+++ src/test/controller/IfController.ts
@@ -4,10 +4,12 @@ import { IF } from '@/decorators/if';
 import { ResponseHeader } from '@/decorators/response';
 
 @Singleton()
-@(IF(!!process.env.__IF__STATE__, Controller('/if')).ELSE(Controller('/else')).ENDIF())
+@(IF(process.env.__IF__STATE__ === 'true', Controller('/if'))
+	.ELSE(Controller('/else'))
+	.ENDIF())
 export class IfController {
 	@HttpMethod.Get('/test')
-	@(IF(!!process.env.__IF__STATE__, ResponseHeader('X-Test', '123')).ENDIF())
+	@(IF(process.env.__IF__STATE__ === 'true', ResponseHeader('X-Test', '123')).ENDIF())
 	async test() {
 		return 'test';
 	}
