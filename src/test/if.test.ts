09b53372cd650979f4514a015e64be820caf85b2
wangyige0701 <1776686515@qq.com>
2026-03-23 21:45:53 +0800

    条件控制器完善

--- src/test/if.test.ts
+++ src/test/if.test.ts
@@ -6,61 +6,46 @@ import { fileURLToPath } from 'url';
 import { decorator } from '@/index';
 
 describe('IF', () => {
+	function createApp() {
+		vi.resetModules();
+		const app = new Koa();
+		app.use(
+			decorator({
+				controllerDir: path.resolve(path.dirname(fileURLToPath(import.meta.url)), './controller'),
+				allowedMethods: true,
+			}),
+		);
+		return app;
+	}
+
 	afterEach(() => {
 		vi.unstubAllEnvs();
 	});
 
 	it('should return test', async () => {
-		vi.stubGlobal('process.env', {
-			__IF__STATE__: true,
-		});
+		vi.stubEnv('__IF__STATE__', 'true');
 
-		const app = new Koa();
-		app.use(
-			decorator({
-				controllerDir: path.resolve(path.dirname(fileURLToPath(import.meta.url)), './controller'),
-				allowedMethods: true,
-			}),
-		);
-
+		const app = createApp();
 		const res = await request(app.callback()).get('/if/test');
-		expect(res.body).toBe('test');
+		expect(res.text).toBe('test');
 		expect(res.status).toBe(200);
 		expect(res.headers['x-test']).toBe('123');
 	});
 
 	it('should be 404', async () => {
-		vi.stubGlobal('process.env', {
-			__IF__STATE__: false,
-		});
+		vi.stubEnv('__IF__STATE__', 'false');
 
-		const app = new Koa();
-		app.use(
-			decorator({
-				controllerDir: path.resolve(path.dirname(fileURLToPath(import.meta.url)), './controller'),
-				allowedMethods: true,
-			}),
-		);
-
+		const app = createApp();
 		const res = await request(app.callback()).get('/if/test');
 		expect(res.status).toBe(404);
 	});
 
 	it('should return test with else', async () => {
-		vi.stubGlobal('process.env', {
-			__IF__STATE__: false,
-		});
+		vi.stubEnv('__IF__STATE__', 'false');
 
-		const app = new Koa();
-		app.use(
-			decorator({
-				controllerDir: path.resolve(path.dirname(fileURLToPath(import.meta.url)), './controller'),
-				allowedMethods: true,
-			}),
-		);
-
+		const app = createApp();
 		const res = await request(app.callback()).get('/else/test');
 		expect(res.status).toBe(200);
-		expect(res.body).toBe('test');
+		expect(res.text).toBe('test');
 	});
 });
