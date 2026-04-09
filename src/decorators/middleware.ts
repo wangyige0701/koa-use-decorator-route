f23de07df0941919bd8f9b0137f7bcdfeaca6bd3
Wang Yige <1776686515@qq.com>
2026-04-09 15:13:40 +0800

    中间件函数优化

--- src/decorators/middleware.ts
+++ src/decorators/middleware.ts
@@ -1,32 +1,39 @@
-import { ROUTE_MIDDLEWARES } from '@/config';
 import type { Middleware } from 'koa';
+import { ROUTE_MIDDLEWARES } from '@/config';
 
 /**
  * 创建一个中间件装饰器
  *
  * Create a middleware decorator.
  *
- * @param callback 中间件函数 / middleware function
+ * @param middlewares 中间件函数 / middleware functions
  */
-export function createMiddlewareDecorator(callback: Middleware): ClassDecorator;
-export function createMiddlewareDecorator(callback: Middleware): MethodDecorator;
-export function createMiddlewareDecorator(callback: Middleware): ClassDecorator | MethodDecorator {
+export function createMiddlewareDecorator(...middlewares: Middleware[]): ClassDecorator;
+export function createMiddlewareDecorator(...middlewares: Middleware[]): MethodDecorator;
+export function createMiddlewareDecorator(...middlewares: Middleware[]): ClassDecorator | MethodDecorator {
 	return (target: any, propertyKey?: string | symbol, descriptor?: PropertyDescriptor) => {
 		if (!propertyKey) {
-			const middlewares = (Reflect.getMetadata(ROUTE_MIDDLEWARES, target) || []) as Middleware[];
-			middlewares.push(callback);
-			Reflect.defineMetadata(ROUTE_MIDDLEWARES, middlewares, target);
+			const middlewaresList = (Reflect.getMetadata(ROUTE_MIDDLEWARES, target) || []) as Middleware[];
+			middlewaresList.push(...middlewares);
+			Reflect.defineMetadata(ROUTE_MIDDLEWARES, middlewaresList, target);
 			return target;
 		}
 
-		const middlewares = (Reflect.getMetadata(ROUTE_MIDDLEWARES, target.constructor, propertyKey) ||
+		const middlewaresList = (Reflect.getMetadata(ROUTE_MIDDLEWARES, target.constructor, propertyKey) ||
 			[]) as Middleware[];
-		middlewares.push(callback);
-		Reflect.defineMetadata(ROUTE_MIDDLEWARES, middlewares, target.constructor, propertyKey);
+		middlewaresList.push(...middlewares);
+		Reflect.defineMetadata(ROUTE_MIDDLEWARES, middlewaresList, target.constructor, propertyKey);
 		return descriptor;
 	};
 }
 
-export function Middleware(callback: Middleware): ClassDecorator | MethodDecorator {
-	return createMiddlewareDecorator(callback);
+/**
+ * 路由中间件装饰器
+ *
+ * Route middleware decorator.
+ *
+ * @param middlewares 中间件函数 / middleware functions
+ */
+export function RouteMiddleware(...middlewares: Middleware[]): ClassDecorator | MethodDecorator {
+	return createMiddlewareDecorator(...middlewares);
 }
