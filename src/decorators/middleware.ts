69d6ed81c4606e2a3b68df7ed9b49aec88d472ca
wangyige0701 <1776686515@qq.com>
2026-04-09 21:44:24 +0800

    实现修复

--- src/decorators/middleware.ts
+++ src/decorators/middleware.ts
@@ -2,28 +2,29 @@ import type { Middleware } from 'koa';
 import { ROUTE_MIDDLEWARES } from '@/config';
 
 /**
- * 创建一个中间件装饰器
+ * 创建一个中间件装饰器工厂
  *
- * Create a middleware decorator.
+ * Create a middleware decorator factory.
  *
  * @param middlewares 中间件函数 / middleware functions
  */
-export function createMiddlewareDecorator(...middlewares: Middleware[]): ClassDecorator;
-export function createMiddlewareDecorator(...middlewares: Middleware[]): MethodDecorator;
-export function createMiddlewareDecorator(...middlewares: Middleware[]): ClassDecorator | MethodDecorator {
-	return (target: any, propertyKey?: string | symbol, descriptor?: PropertyDescriptor) => {
-		if (!propertyKey) {
-			const middlewaresList = (Reflect.getMetadata(ROUTE_MIDDLEWARES, target) || []) as Middleware[];
-			middlewaresList.push(...middlewares);
-			Reflect.defineMetadata(ROUTE_MIDDLEWARES, middlewaresList, target);
-			return target;
-		}
+export function createMiddlewareDecorator(...middlewares: Middleware[]) {
+	// TODO 保留支持传参的中间件装饰器工厂
+	return () => {
+		return (target: any, propertyKey?: string | symbol, descriptor?: PropertyDescriptor) => {
+			if (!propertyKey) {
+				const middlewaresList = (Reflect.getMetadata(ROUTE_MIDDLEWARES, target) || []) as Middleware[];
+				middlewaresList.push(...middlewares);
+				Reflect.defineMetadata(ROUTE_MIDDLEWARES, middlewaresList, target);
+				return target;
+			}
 
-		const middlewaresList = (Reflect.getMetadata(ROUTE_MIDDLEWARES, target.constructor, propertyKey) ||
-			[]) as Middleware[];
-		middlewaresList.push(...middlewares);
-		Reflect.defineMetadata(ROUTE_MIDDLEWARES, middlewaresList, target.constructor, propertyKey);
-		return descriptor;
+			const middlewaresList = (Reflect.getMetadata(ROUTE_MIDDLEWARES, target.constructor, propertyKey) ||
+				[]) as Middleware[];
+			middlewaresList.push(...middlewares);
+			Reflect.defineMetadata(ROUTE_MIDDLEWARES, middlewaresList, target.constructor, propertyKey);
+			return descriptor;
+		};
 	};
 }
 
@@ -34,6 +35,6 @@ export function createMiddlewareDecorator(...middlewares: Middleware[]): ClassDe
  *
  * @param middlewares 中间件函数 / middleware functions
  */
-export function RouteMiddleware(...middlewares: Middleware[]): ClassDecorator | MethodDecorator {
-	return createMiddlewareDecorator(...middlewares);
+export function RouteMiddleware(...middlewares: Middleware[]) {
+	return createMiddlewareDecorator(...middlewares)();
 }
